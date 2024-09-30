30fda4462883c1e945eb703c5587b53ae9e2b065
Angelica Norlander <angelicanorlander@telia.com>
2024-09-30 12:06:51 +0200

    La in en if-sats för att göra så att man inte kan lägga in fel symboler.

--- Uppgift 5/Program.cs
+++ Uppgift 5/Program.cs
@@ -11,10 +11,25 @@ namespace EnkelKalkylator
             Console.WriteLine("Enkel Kalkylator");
             Console.WriteLine("Ange operation (+, -, *, /): ");
             char operation = Console.ReadKey().KeyChar;
+
+            //Programmet ska inte funka om man skriver in fel tecken. 
+            if (operation != '+' && operation != '-' && operation != '*' && operation != '/')
+            {
+                Console.WriteLine("Nu blev det fel. Programmet avslutas");
+                return;
+                
+            }
+
+
+
+
             Console.WriteLine("Ange första talet: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
+            
             Console.WriteLine("Ange andra talet:");
             double num2 = Convert.ToDouble(Console.ReadLine());
+
+
 
             double result = 0;
 
